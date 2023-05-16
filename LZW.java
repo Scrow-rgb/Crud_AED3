812012afac6898240ca99de5ee39dd20bcb044fe
Scrow-rgb <macrinho32@gmail.com>
2023-05-16 16:13:27 -0300

    update no dicionário

--- LZW.java
+++ LZW.java
@@ -17,7 +17,7 @@ public class LZW {
         char[] iniciarDicionario = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-                'V', 'W', 'X', 'Y', 'Z', ' ', '(', ')'};
+                'V', 'W', 'X', 'Y', 'Z', ' ', '(', ')', '-'};
 
         // Inicializar o dicionário com os caracteres ASCII
         for (int i = 0; i < iniciarDicionario.length; i++) {
